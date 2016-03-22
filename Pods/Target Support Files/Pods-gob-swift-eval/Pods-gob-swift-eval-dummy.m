fda7821c1b42f89790168024c3e2be286f0c1016
Etienne Dldc <etienne.dldc@outlook.fr>
2016-03-22 18:01:19 +0100

    init pod

---
+++ Pods/Target Support Files/Pods-gob-swift-eval/Pods-gob-swift-eval-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gob_swift_eval : NSObject
+@end
+@implementation PodsDummy_Pods_gob_swift_eval
+@end
---
+++ Pods/Target Support Files/Pods-gob-swift-eval/Pods-gob-swift-eval-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_gob_swift_evalVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_gob_swift_evalVersionString[];
+
